b1b920a87238b62e050f92dfe1639cca8af4d86b
jimstroming <ajs061@motorola.com>
2016-08-22 21:42:34 -0500

    define the angry ram riddler problem.

---
+++ angryram.cpp
@@ -0,0 +1,31 @@
+/* From http://fivethirtyeight.com/features/can-you-outrun-the-angry-ram-coming-right-for-oh-god/
+
+You, a hard-driving sheep farmer, are tucked into the southeast corner of your square, 
+fenced-in sheep paddock. There are two gates equidistant from you: one at the 
+southwest corner and one at the northeast corner. An angry, recalcitrant ram 
+enters the paddock from the southwest gate and charges directly at you at a 
+constant speed. You run — obviously! — at a constant speed along the eastern 
+fence toward the northeast gate in an attempt to escape. The ram keeps charging, 
+always directly at you.
+
+How much faster than you does the ram have to run so that he catches you 
+just as you reach the gate?
+
+*/
+
+
+
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+}
+
+
+
+
