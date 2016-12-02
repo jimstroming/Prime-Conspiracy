4235b958aab8497afbed07c03501659d9428fc74
Jim Stroming <jimstroming@gmail.com>
2016-12-02 15:08:48 -0600

    start converting the baseball riddler from python to c++

---
+++ riddler_baseball.cpp
@@ -0,0 +1,25 @@
+/* ========================================================================== */
+/*                                                                            */
+/*   Filename.cpp                                                             */
+/*   (c) 2012 Author                                                          */
+/*  
+from http://fivethirtyeight.com/features/can-you-solve-the-puzzle-of-the-baseball-division-champs/
+
+Assume you have a sport (lets call it baseball) in which each team plays 162 games in 
+a season. Assume you have a division of five teams (call it the AL East) where each 
+team is of exact equal ability. Specifically, each team has a 50 percent chance of 
+winning each game. What is the expected value of the number of wins for the team that
+finishes in first place?
+
+Need to make an assumption about interdivision vs intradivision games.
+Lets start with the way the schedule is done currently.
+19 games against each of your 4 division opponents.
+86 games outside your division.
+
+/* ========================================================================== */
+
+
+void main()
+{
+
+}
